c4777393ab5281ce6cf5e4230620da83c6f084a6
mariama sanneh <masan@MacBook-Pro-de-mariama.local>
2026-01-27 11:24:26 +0100

    finalisation de chronologie et czracteristique

---
+++ adapage-projet-wakfu/src/DATA/caracteristique.js
@@ -0,0 +1,47 @@
+
+
+export const caracteristique=[{
+    id:0,
+physique:{titre:" description physique",
+genre:"masculin",
+cheveux: "roux",
+apparence:"Goultard possède les caractéristiques physiques propres aux Iops comme les cheveux roux, les oreilles pointues et la tenue rouge et blanche des iops. Ses cheveux sont beaucoup plus longs, atteignant presque ses pieds, et sont coiffés en queue de cheval avec deux mèches sur chaque côté. À l'ère du Wakfu, il porte une barbe taillée. Il est torse nu, sa tenue étant déchirée.Quand il était possédé par Médoroziam, sa peau était très sombre avec des marques blanches et ses cheveux blancs. Sous sa forme de Vald, il reprend une apparence de iop normal, et sous sa forme Dark Vlad il porte une armure noire et a une tête squelettique avec des cheveux en feu.",
+},
+  
+personnalite:{titre:" personnalité",
+    sagesse:"",
+    loyauté:"",
+    inspirant:"solitaire mais inspirant:Il préfère souvent agir seul, mais inspire profondément ceux qui le rencontrent.",
+    personnalite:"À l'instar de tous les iops, Goultard est un grand amoureux des combats et n'est pas très intelligent. Cependant il sait faire preuve de sagesse et de recul et est prêt à tout pour protéger les siens. Lorsqu'il était possédé, il était devenu un être sanguinaire et plus violent, dénué de toute forme d'empathie avant sa rencontre avec Arty. Seuls ceux qu'il considérait comme des guerriers dignes obtenaient son respect, méprisant tout autre forme de morale.En tant que Vald, il avait la personnalité d'un jeune iop immature et amateur de combat, où il retrouvait le plaisir de la découverte et de l'aventure. Tandis qu'en tant que Dark Vlad il était psychotique, violent et prenait plaisir dans la destruction.En raison de son passé polyvalent, Goultard est fermement convaincu que tout le monde à droit à une seconde chance, étant donné qu'il a pu se racheter et redevenir un héro en participant à la défaite du Cornu Mollu, malgré tout ses crimes commis en tant que Goultard le Barbare ou Dark Vlad. Certains éléments de son histoire semblent toujours le hanter à l'ère du Wakfu, comme les destructions causées par le Dark Vlad, et le soupçon de rancune qu'il garde envers son père, le dieu Iop, pour avoir été absent la majorité de sa vie. A l'ère Waven, il est sous-entendu qu'il n'a pas oublié la perte de sa famille."
+},
+
+pouvoir:{titre:"Pouvoir et capacités",
+    immortalité:"Considéré comme le dernier grand guerrier de la lignée Goultard encore actif dans le Monde des Douze.",
+    avatar:{
+    descpt:"Suite à des circonstances différentes, Goultard possède, en plus de lui même, deux autres personnalités qui habitent le même corps. Goultard est au courant de leur existence.",
+    avatars:[{titre:"Dark Vlad",
+            descpt:"Cette personnalité ne possède que 50% de la puissance de Goultard, bien qu'elle puisse être tout aussi dangereuse en raison de sa grande cruauté. Elle est née lors de la première invasion des Shushus, lors ce que la magicienne Algathe, amante de Goultard, s'est faite tuée sous ses yeux. Le Dark Vlad a alors massacré tout le monde, que ce soit les Shushus ou les villageois qu'il était sensé protéger, en emportant avec lui le Dofus Emeraude. Ce Iop aux cheveux enflammés possède une épée qui l'est tout autant, est extrêmement rapide et sa soif de sang le rend capable de détruire une ville entière. Niveau intellect, il est cependant bien inferieur à sa personnalité originel, s'exprimant souvent dans un langage approximatif, et sa personnalité frénétique et fonceuse font qu'il peut se laisser surprendre. Lors de sa réémergence à l'ère du Wakfu, le Dark Vlad semble au courant de l'existence de Goultard."},
+            {titre:"Vlad",
+            descpt:"Cette personnalité se contente de 10% de la puissance de Goultard. C'est un jeune Iop assez banal, c'est à dire assez peu malin, fonceur et bagarreur. Il est aussi maladroit et se retrouve souvent en difficulté. Il est cependant candide. Cette personnalité née à l'âge des Dofus permet surtout à Goultard, qui à l'époque avait perdu toutes ses émotions humaines, de retrouver la sensation des premières fois, de partir à l'aventure pour découvrir le monde, et de faire la rencontre d'Arty, qui deviendra son plus proche compagnon, et lui fera retrouver le sentiment d'amitié. Vlad est également le seul à ne pas être au courant de l'existence des deux autres personnalités."}]
+    },
+    dieu:{titre:"Pouvoirs du dieu Iop",
+        descrpt:" Lorsque le dieu Iop, le père de Goultard, a décidé de se réincarner en mortel, il a confié ses pouvoirs à son fils. En 988, Goultard retransmet ses pouvoirs à la réincarnation de son père, Tristepin, qui part alors affronter Ogrest, mais les récupère après que son disciple ai perdu son bras gauche lors de l'affrontement. Face au refus de Tristepin de reprendre ses pouvoirs, Goultard demeure alors le plus puissant des dieux."
+},force:{titre:"force et endurance:",
+    descrpt:"Même après des décennies de combats, Goultard reste extrêmement puissant.",
+}
+},
+legende:{titre:"Légende et influence",
+    descrpt1:"Considéré comme le dernier grand guerrier de la lignée Goultard encore actif dans le Monde des Douze.",
+    descrpt2:"Sa réputation est telle que les dieux eux-mêmes respectent sa force et son jugement.",
+    descrpt3:"Les jeunes héros voient en lui l’incarnation de la bravoure et de l’honneur, perpétuant la légende des Goultard.",
+},
+exploit:[{titre:"Protection des territoires",
+        descrpt1:"Surveille et défend des villages, forêts et cités contre les menaces : monstres, bandits et perturbateurs d’équilibre.",
+         descrpt2:"Sa présence seule dissuade de nombreuses attaques.",
+},{titre:"Mentorat des héros",
+    descrpt1:"Guide les aventuriers et personnages clés dans leurs quêtes.",
+  descrpt2:"Partage son expérience de guerrier légendaire pour former de nouvelles générations.",},{
+    titre:"Interventions légendaires",
+    descrpt1:"Intervient dans des quêtes majeures de Wakfu, souvent en allié puissant mais discret.",
+  descrpt2:"Protège les reliques ou artefacts anciens, comme certains objets de la famille Goultard (ex : Épée Divine)."}],
+}];
