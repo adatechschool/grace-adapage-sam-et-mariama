3113d64006e382c42aae13de2e3d24180fbff7bf
mariama sanneh <masan@MacBook-Pro-de-mariama.local>
2026-01-28 16:05:29 +0100

    ajout couleur et background

--- adapage-projet-wakfu/src/components/Footer.tsx
+++ adapage-projet-wakfu/src/components/Footer.tsx
@@ -2,9 +2,14 @@
 
 function Footer() {
   return (
-    <footer className="text-xl text-center" >
-      <p>© SAM&Mariama2026 — Projet Wakfu Goultard</p>
+     <div className="w-full bg-gray-900">
+    <footer className="text-xl text-center text-gray-50" >
+      
+    <span className="text-center text-gray-50">Mentions légales</span>
+    <p className="text-center text-gray-50">Contact</p> 
+    <p>© SAM&Mariama2026 — Projet Wakfu Goultard</p> 
     </footer>
+     </div>
   );
 }
 
