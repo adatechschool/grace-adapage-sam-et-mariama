95c52ae74ab7b379c360732239908d3c4c9bdae7
mariama sanneh <masan@MacBook-Pro-de-mariama.local>
2026-01-29 11:40:53 +0100

    faire stylisation de caracteristique et ankamaverse

--- adapage-projet-wakfu/src/DATA/caracteristique.tsx
+++ adapage-projet-wakfu/src/DATA/caracteristique.tsx
@@ -3,16 +3,16 @@
 export const caracteristique=[{
     id:0,
     
-physique:{titre:" description physique",
+physique:{titre:" Description physique",
 genre:"masculin",
 cheveux: "roux",
 apparence:"Goultard possède les caractéristiques physiques propres aux Iops comme les cheveux roux, les oreilles pointues et la tenue rouge et blanche des iops. Ses cheveux sont beaucoup plus longs, atteignant presque ses pieds, et sont coiffés en queue de cheval avec deux mèches sur chaque côté. À l'ère du Wakfu, il porte une barbe taillée. Il est torse nu, sa tenue étant déchirée.Quand il était possédé par Médoroziam, sa peau était très sombre avec des marques blanches et ses cheveux blancs. Sous sa forme de Vald, il reprend une apparence de iop normal, et sous sa forme Dark Vlad il porte une armure noire et a une tête squelettique avec des cheveux en feu.",
 },
   
-personnalite:{titre:" personnalité",
+personnalite:{titre:" Personnalité",
     sagesse:"",
     loyauté:"",
-    inspirant:"solitaire mais inspirant:Il préfère souvent agir seul, mais inspire profondément ceux qui le rencontrent.",
+    inspirant:"Solitaire mais inspirant. Il préfère souvent agir seul, mais inspire profondément ceux qui le rencontrent.",
     personnalite:"À l'instar de tous les iops, Goultard est un grand amoureux des combats et n'est pas très intelligent. Cependant il sait faire preuve de sagesse et de recul et est prêt à tout pour protéger les siens. Lorsqu'il était possédé, il était devenu un être sanguinaire et plus violent, dénué de toute forme d'empathie avant sa rencontre avec Arty. Seuls ceux qu'il considérait comme des guerriers dignes obtenaient son respect, méprisant tout autre forme de morale.En tant que Vald, il avait la personnalité d'un jeune iop immature et amateur de combat, où il retrouvait le plaisir de la découverte et de l'aventure. Tandis qu'en tant que Dark Vlad il était psychotique, violent et prenait plaisir dans la destruction.En raison de son passé polyvalent, Goultard est fermement convaincu que tout le monde à droit à une seconde chance, étant donné qu'il a pu se racheter et redevenir un héro en participant à la défaite du Cornu Mollu, malgré tout ses crimes commis en tant que Goultard le Barbare ou Dark Vlad. Certains éléments de son histoire semblent toujours le hanter à l'ère du Wakfu, comme les destructions causées par le Dark Vlad, et le soupçon de rancune qu'il garde envers son père, le dieu Iop, pour avoir été absent la majorité de sa vie. A l'ère Waven, il est sous-entendu qu'il n'a pas oublié la perte de sa famille."
 },
 
@@ -26,8 +26,8 @@ pouvoir:{titre:"Pouvoir et capacités",
             descpt:"Cette personnalité se contente de 10% de la puissance de Goultard. C'est un jeune Iop assez banal, c'est à dire assez peu malin, fonceur et bagarreur. Il est aussi maladroit et se retrouve souvent en difficulté. Il est cependant candide. Cette personnalité née à l'âge des Dofus permet surtout à Goultard, qui à l'époque avait perdu toutes ses émotions humaines, de retrouver la sensation des premières fois, de partir à l'aventure pour découvrir le monde, et de faire la rencontre d'Arty, qui deviendra son plus proche compagnon, et lui fera retrouver le sentiment d'amitié. Vlad est également le seul à ne pas être au courant de l'existence des deux autres personnalités."}]
     },
     dieu:{titre:"Pouvoirs du dieu Iop",
-        descrpt:" Lorsque le dieu Iop, le père de Goultard, a décidé de se réincarner en mortel, il a confié ses pouvoirs à son fils. En 988, Goultard retransmet ses pouvoirs à la réincarnation de son père, Tristepin, qui part alors affronter Ogrest, mais les récupère après que son disciple ai perdu son bras gauche lors de l'affrontement. Face au refus de Tristepin de reprendre ses pouvoirs, Goultard demeure alors le plus puissant des dieux."
-},force:{titre:"force et endurance:",
+        descrpt:"Lorsque le dieu Iop, le père de Goultard, a décidé de se réincarner en mortel, il a confié ses pouvoirs à son fils. En 988, Goultard retransmet ses pouvoirs à la réincarnation de son père, Tristepin, qui part alors affronter Ogrest, mais les récupère après que son disciple ai perdu son bras gauche lors de l'affrontement. Face au refus de Tristepin de reprendre ses pouvoirs, Goultard demeure alors le plus puissant des dieux."
+},force:{titre:"Force et endurance:",
     descrpt:"Même après des décennies de combats, Goultard reste extrêmement puissant.",
 }
 },
--- adapage-projet-wakfu/src/Pages/Caracteristiques.tsx
+++ adapage-projet-wakfu/src/Pages/Caracteristiques.tsx
@@ -64,50 +64,68 @@ interface Caracteristique {
 }
 
 function Caracteristiques() {
-  return (
+ return (
     <>
       <Header />
+
       <h1 className="text-xl text-center font-bold text-gray-500 mb-4">
         Caractéristiques
       </h1>
 
-      {caracteristique.map((Caract) => (
-        <div key={Caract.id}>
-          <h2 className="p-6">{Caract.physique.titre}</h2>
-          <p className="p-2">{Caract.physique.genre}</p>
-          <p className="p-2">{Caract.physique.cheveux}</p>
-          <p className="p-2">{Caract.physique.apparence}</p>
+      {/* Centrer la colonne */}
+      <div className="flex justify-center">
+        <div className="w-3/4 flex flex-col gap-6">
+          {caracteristique.map((Caract) => (
+            <div key={Caract.id} className="flex flex-col gap-6">
 
-          {Caract.personnalite && (
-            <>
-              <h3 className="p-6">{Caract.personnalite.titre}</h3>
-              <p className="p-2">{Caract.personnalite.sagesse}</p>
-              <p className="p-2">{Caract.personnalite.loyauté}</p>
-              <p className="p-2">{Caract.personnalite.inspirant}</p>
-              <p className="p-2">{Caract.personnalite.personnalite}</p>
-            </>
-          )}
+              {/* catrte01 — Physique + Personnalité */}
+              <div className="p-6 bg-gray-100 rounded-xl shadow">
+                
+                <h2 className="p-6">{Caract.physique.titre}</h2>
+                <p className="p-2">Genre : {Caract.physique.genre}</p>
+                <p className="p-2">Couleur de cheveux : {Caract.physique.cheveux}</p>
+                <p className="p-2">Physique & apparence : {Caract.physique.apparence}</p>
 
-          {Caract.pouvoir && (
-            <div className="px-6">
-              <h3 className="p-6">{Caract.pouvoir.titre}</h3>
-              <p className="p-2">{Caract.pouvoir.immortalité}</p>
-
-              <div>
-                {Caract.pouvoir.avatar.avatars.map((a, idx) => (
-                  <p key={idx} className="p-2">
-                    <strong className="p-4">{a.titre} :</strong> {a.descpt}
-                  </p>
-                ))}
+                {Caract.personnalite && (
+                  <>
+                    <h3 className="p-6">{Caract.personnalite.titre}</h3>
+                    <p className="p-2">{Caract.personnalite.sagesse}</p>
+                    <p className="p-2">{Caract.personnalite.loyauté}</p>
+                    <p className="p-2">{Caract.personnalite.inspirant}</p>
+                    <p className="p-2">{Caract.personnalite.personnalite}</p>
+                  </>
+                )}
               </div>
 
-              <h3 className="p-6">{Caract.pouvoir.dieu.titre}</h3>
-              <p className="p-2">{Caract.pouvoir.dieu.descrpt}</p>
-              <h3 className="p-6">{Caract.pouvoir.force.titre}</h3>
-              <p className="p-2">{Caract.pouvoir.force.descrpt}</p>
+              {/* carte02 — Pouvoir */}
+              {Caract.pouvoir && (
+                <div className="p-6 bg-gray-50 rounded-xl shadow">
+                
+                  <div className="px-6">
+                    <h3 className="p-6">{Caract.pouvoir.titre}</h3>
+                    <p className="p-2">{Caract.pouvoir.immortalité}</p>
 
+                    <div>
+                      {Caract.pouvoir.avatar.avatars.map((a, idx) => (
+                        <p key={idx} className="p-2">
+                          <strong className="p-4">{a.titre} :</strong> {a.descpt}
+                        </p>
+                      ))}
+                    </div>
+
+                    <h3 className="p-6">{Caract.pouvoir.dieu.titre}</h3>
+                    <p className="p-2">{Caract.pouvoir.dieu.descrpt}</p>
+
+                    <h3 className="p-6">{Caract.pouvoir.force.titre}</h3>
+                    <p className="p-2">{Caract.pouvoir.force.descrpt}</p>
+                  </div>
+                </div>
+              )}
+
+              {/* carte03 — Légende */}
               {Caract.legende && (
-                <div className="p-6">
+                <div className="p-6 bg-gray-50 rounded-xl shadow">
+                  
                   <h3 className="p-6">{Caract.legende.titre}</h3>
                   <p className="p-2">{Caract.legende.descrpt1}</p>
                   <p className="p-2">{Caract.legende.descrpt2}</p>
@@ -115,8 +133,10 @@ function Caracteristiques() {
                 </div>
               )}
 
+              {/* carte04 — Exploits */}
               {Caract.exploit && Caract.exploit.length > 0 && (
-                <div className="p-6">
+                <div className="p-6 bg-gray-50 rounded-xl shadow">
+                  
                   {Caract.exploit.map((a, idx) => (
                     <div key={idx} className="mb-3">
                       <h3 className="p-6">{a.titre}</h3>
@@ -126,10 +146,11 @@ function Caracteristiques() {
                   ))}
                 </div>
               )}
+
             </div>
-          )}
+          ))}
         </div>
-      ))}
+      </div>
 
       <Footer />
     </>
