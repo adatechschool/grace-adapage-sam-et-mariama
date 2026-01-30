938ea0efa217006aed949de13d8237882e3b7ca0
mariama sanneh <masan@MacBook-Pro-de-mariama.local>
2026-01-30 14:06:05 +0100

    finalisation ankaverse

--- adapage-projet-wakfu/src/DATA/ankamaVerse.tsx
+++ adapage-projet-wakfu/src/DATA/ankamaVerse.tsx
@@ -1,16 +1,21 @@
 const ankamaVerse =[{
     id:0,
-    name:"Goultard l’Ancien",
+    name:"Dieu Iop",
     label:"Fondateur de la lignée Goultard, Guerrier légendaire, connu pour avoir uni plusieurs clans barbares et repoussé des envahisseurs.Ses exploits posent les bases de la réputation de la famille.",
      image:""
 
 },{  id:1,
-    name:"Goultard le Jeune (DOFUS)",
-    label:"Descendants formés dès l’enfance à l’art du combat.Interviennent dans des guerres locales et protègent la lignée.Quelques membres ont été mentionnés dans des quêtes ou légendes mineures dans Dofus.",
+    name:"Le premier Goultard et sa lignée",
+    label:"Goultard l’Ancien, guerrier légendaire et fondateur de la lignée Goultard, unifia plusieurs clans barbares et repoussa les envahisseurs, posant les bases de la réputation de sa famille. Connu pour son courage et sa capacité à inspirer ses alliés, il demeure un symbole de bravoure et de force.",
     image:""
 },{  id:2,
     name:"Goultard le Protecteur",
-    label:"Toujours vivant, même après les grandes guerres.Mentor pour certains héros et joueurs.Continue à défendre certaines zones du Monde des Douze.Sa légende inspire encore de nouveaux aventuriers.",
+    label:"Courageux et loyal, agit selon un code d’honneur strict et coordonne ses combats avec stratégie, faisant de lui un leader redoutable.",
+    image:""
+},
+{  id:3,
+    name:"DARK VALD",
+    label:"impitoyable et obsessionnel, agit avec brutalité pour atteindre ses objectifs et suit ses propres codes, renforçant son aura redoutable.",
     image:""
 }];
 
--- adapage-projet-wakfu/src/components/Footer.tsx
+++ adapage-projet-wakfu/src/components/Footer.tsx
@@ -1,22 +1,75 @@
+import { useState } from "react";
 import logoLeft from "../assets/logo-footer/Wakfu_Logo.png";
 import logoCenter from "../assets/logo-footer/Ankama_Games_Logo_blanc.png";
 import logoRight from "../assets/logo-footer/dofus-logo.png";
 
 function Footer() {
+  const [contactOpen, setContactOpen] = useState(false);
+
   return (
-    <footer className="bg-gray-900 text-gray-50 py-6">
-      {/* Logos sur la même ligne */}
+    <footer className="bg-gray-900 text-gray-50 py-6 relative">
+      
       <div className="max-w-6xl mx-auto flex items-center justify-between px-4 mb-4">
-       <img src={logoLeft} alt="Logo gauche" className="h-10" /> 
-        <img src={logoCenter} alt="Logo centre" className="h-10" />
-        <img src={logoRight} alt="Logo droite" className="h-10" />
+        <a href="https://wakfu.guide/" target="_blank" rel="noopener noreferrer">
+          <img src={logoLeft} alt="Logo gauche" className="h-10" />
+        </a>
+        <a href="https://www.ankama.com" target="_blank" rel="noopener noreferrer">
+          <img src={logoCenter} alt="Logo centre" className="h-10" />
+        </a>
+        <a
+          href="https://www.dofus-retro.com/fr/mmorpg/decouvrir"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logoRight} alt="Logo droite" className="h-10" />
+        </a>
       </div>
 
-      {/* Texte */}
-      <div className="text-center space-y-1">
-        <span className="block font-extralight text-xs">Mentions légales
-        <p>Contact</p>
-        <p>© SAM & Mariama 2026 — Projet Wakfu Goultard</p></span>
+   
+      <div className="text-center  relative">
+        <span className="block font-extralight text-xs">Mentions légales</span>
+
+        <div className="relative inline-block">
+          <p
+            onClick={() => setContactOpen(!contactOpen)}
+            className="cursor-pointer text-yellow-500 hover:underline text-xs"
+          >
+            Info supplémentaire
+          </p>
+
+          {contactOpen && (
+            <div className="absolute left-1/2 transform -translate-x-1/2 mt-1 w-56 bg-gray-800 border border-gray-700 rounded-md shadow-lg z-10">
+              <a
+                href="https://wakfu.fandom.com/fr/wiki/Catégorie:Personnages/Univers"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block px-4 py-2 text-xs text-gray-50 hover:bg-gray-700"
+              >
+                Perso
+              </a>
+              <a
+                href="https://krosmoz.fandom.com/fr/wiki/Krosmoz/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block px-4 py-2 text-xs text-gray-50 hover:bg-gray-700"
+              >
+                Univers
+              </a>
+              <a
+                href="https://www.ankama-shop.com/fr/produits-derives-wakfu/4606-pack-les-legendes-du-krosmoz-dofus-wakfu.html"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block px-4 py-2 text-xs text-gray-50 hover:bg-gray-700"
+              >
+                Jeux / Shop
+              </a>
+            </div>
+          )}
+        </div>
+
+        <span className="block font-extralight text-xs">
+          © SAM & Mariama 2026 — Projet Wakfu Goultard
+        </span>
       </div>
     </footer>
   );
